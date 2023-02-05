dae614535c35e02983580337f4474e55dd34b9ae
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-02-05 12:26:09 -0600

    paypal

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,9 +1,37 @@
+import { loadScript } from "@paypal/paypal-js";
+
+let paypal;
+
+try {
+    paypal = await loadScript({ "client-id": "test" });
+} catch (error) {
+    console.error("failed to load the PayPal JS SDK script", error);
+}
+
+if (paypal) {
+    try {
+        await paypal.Buttons().render("#your-container-element");
+    } catch (error) {
+        console.error("failed to render the PayPal Buttons", error);
+    }
+}
+
+paypal.Buttons({
+    style: {
+      layout: 'vertical',
+      color:  'blue',
+      shape:  'rect',
+      label:  'paypal'
+    }
+  }).render('#paypal-button-container');
 
 
 export default function CheckoutPage(){
     return(
         <div>
-            This is a holder for CheckoutPage.js navigation.
+            This is a holder for CheckoutPage.js navigation.   
         </div>
+
+     
     )
 }
